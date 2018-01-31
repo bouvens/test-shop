b64c5dc45c3733702e84edfe9ce6196e2dc3485e
Alexander Demin <bouvens@gmail.com>
2018-01-31 23:31:25 +0600

    refactor: Move redux connection to <App />

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,14 +1,28 @@
 import React, { Fragment } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import SimpleShop from './simple-shop'
 import DetailedInformation from './detailed-information'
+import * as reducers from '../reducers'
 import { withError } from '../hoc'
 
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(combineReducers({
+    ...reducers,
+    form: formReducer
+}), composeEnhancers(applyMiddleware(thunk)))
+
 export default () => (
-    <BrowserRouter>
-        <Fragment>
-            <Route exact path="/" component={withError(SimpleShop)} />
-            <Route path="/:id" component={withError(DetailedInformation)} />
-        </Fragment>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Fragment>
+                <Route exact path="/" component={withError(SimpleShop)} />
+                <Route path="/:id" component={withError(DetailedInformation)} />
+            </Fragment>
+        </BrowserRouter>
+    </Provider>
 )
--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { reducer as formReducer } from 'redux-form'
-import thunk from 'redux-thunk'
-
-import './index.css'
 import { App } from './containers'
-import * as reducers from './reducers'
+import './index.css'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || // eslint-disable-line no-underscore-dangle
-    compose
-const store = createStore(combineReducers({
-    ...reducers,
-    form: formReducer
-}), composeEnhancers(applyMiddleware(thunk)))
-
-render(
-    (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    ),
-    document.querySelector('#root')
-)
+render(<App />, document.querySelector('#root'))
