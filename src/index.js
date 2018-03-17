a1e96de0933391042d008f331f9961ac9f5abbd7
Alexander Demin <bouvens@gmail.com>
2018-03-17 17:41:46 +0600

    chore: Replace extracted create-react-app with nwb

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom'
 import { App } from './containers'
 import './index.css'
 
-render(<App />, document.querySelector('#root'))
+render(<App />, document.querySelector('#app'))
