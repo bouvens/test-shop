6421343816b9416f69cb71c55736e33ad81f248e
Alexander Demin <bouvens@gmail.com>
2018-04-01 19:42:26 +0600

    refactor: Replace _.map with native methods

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,18 +1,26 @@
-// rule disabled because of false positive on lodash array mapping
-/* eslint-disable react/no-array-index-key */
 import React from 'react'
 import ImmutablePropTypes from 'react-immutable-proptypes'
-import _ from 'lodash'
 import { Link } from 'react-router-dom'
 
-const Article = ({ articles }) => _(articles.toJS())
-    .map(({ title, cost }, id) => (
-        <article key={id}>
-            <h2><Link to={`/${id}`}>{title}</Link></h2>
-            <p>Цена: {cost} ₽</p>
-        </article>
-    ))
-    .value()
+const Article = ({ articles: articlesImmutable }) => {
+    const articles = articlesImmutable.toJS()
+    const output = []
+
+    for (let id in articles) {
+        if (({}).hasOwnProperty.call(articles, id)) {
+            const { title, cost } = articles[id]
+
+            output.push(
+                <article key={id}>
+                    <h2><Link to={`/${id}`}>{title}</Link></h2>
+                    <p>Цена: {cost} ₽</p>
+                </article>
+            )
+        }
+    }
+
+    return output
+}
 
 Article.propTypes = {
     articles: ImmutablePropTypes.map
